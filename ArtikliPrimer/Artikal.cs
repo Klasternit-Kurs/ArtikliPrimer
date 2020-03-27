66226904e6c709d93b6aab08b75e2b07dd8dc008
Dragan Pepic <d.pepic@hotmail.com>
2020-03-27 19:59:36 +0100

    Add project files.

---
+++ ArtikliPrimer/Artikal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtikliPrimer
+{
+	public class Artikal : INotifyPropertyChanged
+	{
+		public string Naziv { get; set; }
+		private decimal cena;
+		public decimal Cena 
+		{
+			get
+			{
+				return cena;
+			}
+
+			set
+			{
+				cena = value;
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Cena"));
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("VrednostNaLageru"));
+			}
+		}
+		private int kolicina;
+		public int Kolicina 
+		{ 
+			get
+			{
+				return kolicina;
+			}
+
+			set
+			{
+				kolicina = value;
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Kolicina"));
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("VrednostNaLageru"));
+			}
+		} 
+
+		public decimal VrednostNaLageru 
+		{
+			get
+			{
+				return Cena * Kolicina; 
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
